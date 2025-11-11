d8fe573eca4502ad004d8d83a2b0462fe829e9d9
StepinIvan <ivan.stepin.99@mail.ru>
2025-11-11 18:37:02 +0700

    Added calculateHydraulicResistance for UniformSuddenAreaChange

--- src/main/java/model/elements/areaChanges/UniformSuddenAreaChange.java
+++ src/main/java/model/elements/areaChanges/UniformSuddenAreaChange.java
@@ -1,25 +1,40 @@
 package model.elements.areaChanges;
 
 import model.elements.bends.BendType;
+import model.elements.utils.AreaChangeCoefficients;
+import model.elements.utils.BendCoefficients;
+import model.elements.utils.TubeCoefficients;
 
 public class UniformSuddenAreaChange extends AreaChange {
+    private final double smallArea;
+    private final double largeArea;
     private final double absolutRoughness;
     private final double re;
 
-    public UniformSuddenAreaChange(double smallerDiameter, double largerDiameter, double absolutRoughness,
+    public UniformSuddenAreaChange(double smallerDiameter, double largerDiameter, double absolutRoughness, double length,
                                    double re) {
         super("Расширение потока с равномерным распределением скоростей", smallerDiameter,
-                largerDiameter);
+                largerDiameter, length);
         this.absolutRoughness = absolutRoughness;
         this.re = re;
+        this.smallArea = Math.PI * Math.pow(smallerDiameter, 2.) / 4.;
+        this.largeArea = Math.PI * Math.pow(largerDiameter, 2.) / 4.;
         validateParameters();
     }
 
     @Override
     public double calculateHydraulicResistance() {
-        double localResistanceCoefficient = 0;
-        double frictionResistanceCoefficient = 0;
-        return localResistanceCoefficient + frictionResistanceCoefficient;
+        double localResistanceCoefficient = AreaChangeCoefficients.calculateUniformSuddenAreaChangeKsiM(re,
+                smallArea / largeArea);
+        double lambda;
+        if (absolutRoughness == 0) {
+            lambda = TubeCoefficients.calculateSmoothPipeLambda(re);
+        } else {
+            lambda = TubeCoefficients.calculateEvenGrainedPipeLambda(re, absolutRoughness / largerDiameter);
+            //TODO Add calculation of friction factor for not even grained roughness;
+        }
+        double frictionResistanceCoefficient = lambda * length / largerDiameter;
+        return localResistanceCoefficient + frictionResistanceCoefficient / Math.pow((largeArea / smallArea), 2.);
     }
 
     @Override
