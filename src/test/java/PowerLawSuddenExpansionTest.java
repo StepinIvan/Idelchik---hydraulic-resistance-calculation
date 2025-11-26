a4cae1bcaece2d8d65e410aef8f7022d32bef831
StepinIvan <ivan.stepin.99@mail.ru>
2025-11-26 20:37:37 +0700

    Finished unit tests for Power law sudden expansion

--- src/test/java/PowerLawSuddenExpansionTest.java
+++ src/test/java/PowerLawSuddenExpansionTest.java
@@ -6,131 +6,67 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PowerLawSuddenExpansionTest {
     @Test
-    public void LocalResistanceLeftBoardUpper () {
+    public void LocalResistanceLeftUpperBoard () {
         PowerLowSuddenExpansion powerLowSuddenExpansion =
                 new PowerLowSuddenExpansion(0.909728368, 12360.77446, 15e-6,
                         1, 0.5);
         double result = powerLowSuddenExpansion.calculateLocalResistanceCoefficient();
         assertEquals(2.6999999848333336, result);
     }
-//    @Test
-//    public void LocalResistanceLeftBoardLower () {
-//        UniformSuddenExpansion uniformSuddenExpansion =
-//                new UniformSuddenExpansion(4, 1,
-//                        15e-6, 1);
-//        double result = uniformSuddenExpansion.calculateLocalResistanceCoefficient(0.001);
-//        assertEquals(3000., result);
-//    }
-//    @Test
-//    public void LocalResistanceRightBoardUpper () {
-//        UniformSuddenExpansion uniformSuddenExpansion =
-//                new UniformSuddenExpansion(1, 4,
-//                        15e-6, 1);
-//        double result = uniformSuddenExpansion.calculateLocalResistanceCoefficient(4000);
-//        assertEquals(0.81, result);
-//    }
-//    @Test
-//    public void LocalResistanceRightBoardLower () {
-//        UniformSuddenExpansion uniformSuddenExpansion =
-//                new UniformSuddenExpansion(4, 1,
-//                        15e-6, 1);
-//        double result = uniformSuddenExpansion.calculateLocalResistanceCoefficient(4000);
-//        assertEquals(0.16, result);
-//    }
-//    @Test
-//    public void LocalResistanceExactValue () {
-//        UniformSuddenExpansion uniformSuddenExpansion =
-//                new UniformSuddenExpansion(1.595769122, 3.568248232,
-//                        15e-6, 1);
-//        double result = uniformSuddenExpansion.calculateLocalResistanceCoefficient(50);
-//        assertEquals(1.6499999996673003, result);
-//    }
-//    @Test
-//    public void LocalResistanceBilinearInterpolation1 () {
-//        UniformSuddenExpansion uniformSuddenExpansion =
-//                new UniformSuddenExpansion(1.784124116, 3.568248232,
-//                        15e-6, 1);
-//        double result = uniformSuddenExpansion.calculateLocalResistanceCoefficient(750);
-//        assertEquals(1.3250000000000002, result);
-//    }
-//    @Test
-//    public void LocalResistanceTest1 () {
-//        UniformSuddenExpansion uniformSuddenExpansion =
-//                new UniformSuddenExpansion(2, 4,
-//                        15e-6, 1);
-//        double result = uniformSuddenExpansion.calculateLocalResistanceCoefficient(10000000);
-//        assertEquals(0.5700000000000001, result);
-//    }
-//    @Test
-//    public void LocalResistanceBilinearInterpolation2 () {
-//        UniformSuddenExpansion uniformSuddenExpansion =
-//                new UniformSuddenExpansion(0.851907589, 1.128379167,
-//                        15e-6, 1);
-//        double result = uniformSuddenExpansion.calculateLocalResistanceCoefficient(42);
-//        assertEquals(1.2400000001416756, result);
-//    }
-//    @Test
-//    public void LocalResistanceLinearInterpolation2 () {
-//        UniformSuddenExpansion uniformSuddenExpansion =
-//                new UniformSuddenExpansion(0.504626504, 1.128379167,
-//                        15e-6, 1);
-//        double result = uniformSuddenExpansion.calculateLocalResistanceCoefficient(700);
-//        assertEquals(1.420000001145616, result);
-//    }
-//    @Test
-//    public void LocalResistanceTest2 () {
-//        UniformSuddenExpansion uniformSuddenExpansion =
-//                new UniformSuddenExpansion(0.504626504, 1.128379167,
-//                        15e-6, 1);
-//        double result = uniformSuddenExpansion.calculateLocalResistanceCoefficient(0.0001);
-//        assertEquals(3000, result);
-//    }
-//    @Test
-//    public void LocalResistanceTest3 () {
-//        UniformSuddenExpansion uniformSuddenExpansion =
-//                new UniformSuddenExpansion(0.504626504, 1.128379167,
-//                        15e-6, 1);
-//        double result = uniformSuddenExpansion.calculateLocalResistanceCoefficient(0.3);
-//        assertEquals(100, result);
-//    }
-//    @Test
-//    public void LocalResistanceTest4 () {
-//        UniformSuddenExpansion uniformSuddenExpansion =
-//                new UniformSuddenExpansion(0.504626504, 1.128379167,
-//                        15e-6, 1);
-//        double result = uniformSuddenExpansion.calculateLocalResistanceCoefficient(0.25);
-//        assertEquals(125, result);
-//    }
-//    @Test
-//    public void LocalResistanceTest5 () {
-//        UniformSuddenExpansion uniformSuddenExpansion =
-//                new UniformSuddenExpansion(0.504626504, 1.128379167,
-//                        15e-6, 1);
-//        double result = uniformSuddenExpansion.calculateLocalResistanceCoefficient(1100000);
-//        assertEquals(0.6400000004868868, result);
-//    }
-//    @Test
-//    public void LocalResistanceLinearInterpolation3 () {
-//        UniformSuddenExpansion uniformSuddenExpansion =
-//                new UniformSuddenExpansion(0.529256743, 1.128379167,
-//                        15e-6, 1);
-//        double result = uniformSuddenExpansion.calculateLocalResistanceCoefficient(50);
-//        assertEquals(1.5999999995746028, result);
-//    }
-//    @Test
-//    public void LocalResistanceTest6 () {
-//        UniformSuddenExpansion uniformSuddenExpansion =
-//                new UniformSuddenExpansion(0.252313252, 1.128379167,
-//                        15e-6, 1);
-//        double result = uniformSuddenExpansion.calculateLocalResistanceCoefficient(50);
-//        assertEquals(1.95, result);
-//    }
-//    @Test
-//    public void LocalResistanceTest7 () {
-//        UniformSuddenExpansion uniformSuddenExpansion =
-//                new UniformSuddenExpansion(0.909728368, 1.128379167,
-//                        15e-6, 1);
-//        double result = uniformSuddenExpansion.calculateLocalResistanceCoefficient(50);
-//        assertEquals(1.05, result);
-//    }
+    @Test
+    public void LocalResistanceLeftLowerBoard () {
+        PowerLowSuddenExpansion powerLowSuddenExpansion =
+                new PowerLowSuddenExpansion(0.909728368, 12360.77446, 15e-6,
+                        1, 1.7976931E308);
+        double result = powerLowSuddenExpansion.calculateLocalResistanceCoefficient();
+        assertEquals(0.9999999914135682, result);
+    }
+    @Test
+    public void LocalResistanceRightUpperBoard () {
+        PowerLowSuddenExpansion powerLowSuddenExpansion =
+                new PowerLowSuddenExpansion(1.909728368, 0.909728368, 15e-6,
+                        1, 0.1);
+        double result = powerLowSuddenExpansion.calculateLocalResistanceCoefficient();
+        assertEquals(0.7, result);
+    }
+    @Test
+    public void LocalResistanceRightLowerBoard () {
+        PowerLowSuddenExpansion powerLowSuddenExpansion =
+                new PowerLowSuddenExpansion(1.909728368, 0.909728368, 15e-6,
+                        1, 1.7976931E308);
+        double result = powerLowSuddenExpansion.calculateLocalResistanceCoefficient();
+        assertEquals(3.878561360970423E-10, result);
+    }
+    @Test
+    public void LocalResistanceExactValue () {
+        PowerLowSuddenExpansion powerLowSuddenExpansion =
+                new PowerLowSuddenExpansion(0.797884561, 1.128379167,
+                        15e-6, 1,3);
+        double result = powerLowSuddenExpansion.calculateLocalResistanceCoefficient();
+        assertEquals(0.4099999996351109, result);
+    }
+    @Test
+    public void LocalResistanceBilinearInterpolation1 () {
+        PowerLowSuddenExpansion powerLowSuddenExpansion =
+                new PowerLowSuddenExpansion(0.529256743, 1.128379167,
+                        15e-6, 1,3.7);
+        double result = powerLowSuddenExpansion.calculateLocalResistanceCoefficient();
+        assertEquals(0.7627999997345523, result);
+    }
+    @Test
+    public void LocalResistanceLinearInterpolation1 () {
+        PowerLowSuddenExpansion powerLowSuddenExpansion =
+                new PowerLowSuddenExpansion(0.529256743, 1.128379167,
+                        15e-6, 1,3.);
+        double result = powerLowSuddenExpansion.calculateLocalResistanceCoefficient();
+        assertEquals(0.81599999971073, result);
+    }
+    @Test
+    public void LocalResistanceLinearInterpolation2 () {
+        PowerLowSuddenExpansion powerLowSuddenExpansion =
+                new PowerLowSuddenExpansion(0.504626504, 1.128379167,
+                        15e-6, 1,3.7);
+        double result = powerLowSuddenExpansion.calculateLocalResistanceCoefficient();
+        assertEquals(0.7940000005241193, result);
+    }
 }
