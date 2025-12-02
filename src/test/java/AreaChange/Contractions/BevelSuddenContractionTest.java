573c3f10bf8d42c4475231434a0fd9a2f48bdca0
StepinIvan <ivan.stepin.99@mail.ru>
2025-12-02 22:49:56 +0700

    Started bevel sudden contraction test

---
+++ src/test/java/AreaChange/Contractions/BevelSuddenContractionTest.java
@@ -0,0 +1,50 @@
+package AreaChange.Contractions;
+
+import model.elements.areaChanges.BevelSuddenContraction;
+import model.elements.areaChanges.ParabolicSuddenExpansion;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BevelSuddenContractionTest {
+    @Test
+    public void LocalResistanceLeftUpperBoard () {
+        BevelSuddenContraction bevelSuddenContraction =
+                new BevelSuddenContraction(1, 1e300,
+                        15e-6, 1,0,0.01);
+        double result = bevelSuddenContraction.calculateLocalResistanceCoefficient();
+        assertEquals(2., result);
+    }
+//    @Test
+//    public void LocalResistanceRightBoard () {
+//        ParabolicSuddenExpansion parabolicSuddenExpansion =
+//                new ParabolicSuddenExpansion(1.2, 1,
+//                        15e-6, 1);
+//        double result = parabolicSuddenExpansion.calculateLocalResistanceCoefficient();
+//        assertEquals(0.34, result);
+//    }
+//    @Test
+//    public void LocalResistanceExactValue () {
+//        ParabolicSuddenExpansion parabolicSuddenExpansion =
+//                new ParabolicSuddenExpansion(1.009253009, 1.128379167,
+//                        15e-6, 1);
+//        double result = parabolicSuddenExpansion.calculateLocalResistanceCoefficient();
+//        assertEquals(0.509999999626242, result);
+//    }
+//    @Test
+//    public void LocalResistanceLinearInterpolation1() {
+//        ParabolicSuddenExpansion parabolicSuddenExpansion =
+//                new ParabolicSuddenExpansion(0.731273279, 1.128379167,
+//                        15e-6, 1);
+//        double result = parabolicSuddenExpansion.calculateLocalResistanceCoefficient();
+//        assertEquals(1.0640000001679866, result);
+//    }
+//    @Test
+//    public void LocalResistanceLinearInterpolation2() {
+//        ParabolicSuddenExpansion parabolicSuddenExpansion =
+//                new ParabolicSuddenExpansion(0.996557497, 1.128379167,
+//                        15e-6, 1);
+//        double result = parabolicSuddenExpansion.calculateLocalResistanceCoefficient();
+//        assertEquals(0.5339999999042387, result);
+//    }
+}
