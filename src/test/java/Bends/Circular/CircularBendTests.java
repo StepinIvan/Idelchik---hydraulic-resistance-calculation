1f817dd54df84c7885330fa31f31ce0933daba2b
StepinIvan <ivan.stepin.99@mail.ru>
2025-12-11 22:48:37 +0700

    kDelta tests started

--- src/test/java/Bends/Circular/CircularBendTests.java
+++ src/test/java/Bends/Circular/CircularBendTests.java
@@ -85,4 +85,101 @@ public class CircularBendTests {
         double result = linePredictionC1.interpolate(5.7);
         assertEquals(0.971, result);
     }
+    @Test
+    public void kDeltaLeftUpperBoard () {
+        double result = BendCoefficients.calculateKDelta(0.4, 10,
+                0.);
+        assertEquals(1.0, result);
+    }
+    @Test
+    public void kDeltaFirstRowTest1() {
+        double result = BendCoefficients.calculateKDelta(0.51, 3500,
+                0.);
+        assertEquals(1.0, result);
+    }
+    @Test
+    public void kDeltaFirstRowTest2() {
+        double result = BendCoefficients.calculateKDelta(0.51, 45000,
+                0.);
+        assertEquals(1.0, result);
+    }
+    @Test
+    public void kDeltaFirstRowTest3() {
+        double result = BendCoefficients.calculateKDelta(0.58, 4000,
+                0.);
+        assertEquals(1.0, result);
+    }
+    @Test
+    public void kDeltaFirstRowTest4() {
+        double result = BendCoefficients.calculateKDelta(0.58, 45000,
+                0.);
+        assertEquals(1.0, result);
+    }
+    @Test
+    public void kDeltaFirstRowTest5() {
+        double result = BendCoefficients.calculateKDelta(0.58, 2000000,
+                0.);
+        assertEquals(1.0, result);
+    }
+    @Test
+    public void kDeltaSecondRowTest1() {
+        double result = BendCoefficients.calculateKDelta(0.52, 3500,
+                0.0001);
+        assertEquals(1.0, result);
+    }
+    @Test
+    public void kDeltaSecondRowTest2() {
+        double result = BendCoefficients.calculateKDelta(0.52, 45000,
+                0.0001);
+        assertEquals(1.05, result);
+    }
+    @Test
+    public void kDeltaSecondRowTest3() {
+        double result = BendCoefficients.calculateKDelta(0.58, 3500,
+                0.0001);
+        assertEquals(1., result);
+    }
+    @Test
+    public void kDeltaSecondRowTest4() {
+        double result = BendCoefficients.calculateKDelta(0.58, 2000000,
+                0.0001);
+        assertEquals(1.1, result);
+    }
+    @Test
+    public void kDeltaThirdRowTest1() {
+        double result = BendCoefficients.calculateKDelta(0.52, 3500,
+                0.01);
+        assertEquals(1., result);
+    }
+    @Test
+    public void kDeltaThirdRowTest2() {
+        double result = BendCoefficients.calculateKDelta(0.52, 45000,
+                0.01);
+        assertEquals(1.5, result);
+    }
+    @Test
+    public void kDeltaThirdRowTest3() {
+        double result = BendCoefficients.calculateKDelta(0.58, 3500,
+                0.01);
+        assertEquals(1., result);
+    }
+    @Test
+    public void kDeltaThirdRowTest4() {
+        double result = BendCoefficients.calculateKDelta(0.58, 45000,
+                0.01);
+        assertEquals(2., result);
+    }
+    @Test
+    public void kDeltaThirdRowTest5() {
+        double result = BendCoefficients.calculateKDelta(0.58, 2000000,
+                0.01);
+        assertEquals(2., result);
+    }
+    //TODO for roughness ratio
+//    @Test
+//    public void kDeltaSecondRowTest4() {
+//        double result = BendCoefficients.calculateKDelta(0.58, 3500,
+//                0.0001);
+//        assertEquals(1., result);
+//    }
 }
